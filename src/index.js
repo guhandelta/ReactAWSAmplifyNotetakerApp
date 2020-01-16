49bab26d8e4b166930a2e3482e23de1acdfc2962
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-01-16 12:44:07 -0600

    Completed configuring Amplify with the React App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports); //To configure all these setup info with Amplify
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
