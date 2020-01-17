a0c9b014e27685796298c313da796134d934c3fa
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-01-17 15:49:07 -0600

    Created a subscriber, to listen/watchout for any notes updated or deleted

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { withAuthenticator } from 'aws-amplify-react'
 
 import { createNote, deleteNote, updateNote } from './graphql/mutations'
 import { listNotes } from './graphql/queries'
-import { onCreateNote } from './graphql/subscriptions'
+import { onCreateNote, onDeleteNote, onUpdateNote } from './graphql/subscriptions'
 
 class App extends React.Component {
   state = {
@@ -17,7 +17,7 @@ class App extends React.Component {
     //Creating a listener in this function will add a listener when the component mounts and removes the listener, on unMount
     this.getNotes();
     this.createNoteListener = API.graphql(graphqlOperation(onCreateNote)).subscribe({
-      //Above statement returns a listener, that listens for any onCreateNode mutation
+      //Above statement returns a listener, that listens for any onCreateNote mutation
       //next is a fn() that allows to get any data returned from the subscription
       next: noteData => {
         const newNote = noteData.value.data.onCreateNote;
@@ -26,6 +26,35 @@ class App extends React.Component {
         this.setState({ notes: updatedNotes });
       }
     });
+    this.deleteNoteListener = API.graphql(graphqlOperation(onDeleteNote)).subscribe({
+      next: noteData => {
+        //Subscribing to a mutation gives the same data, as the response data from the mutation, which is stored in a var to update state
+        const deletedNote = noteData.value.data.onDeleteNote;
+        const updatedNotes = this.state.notes.filter(note => note.id !== deletedNote.id);
+        this.setState({ notes: updatedNotes });
+      }
+    })
+    this.updateNoteListener = API.graphql(graphqlOperation(onUpdateNote)).subscribe({
+      next: noteData => {
+        const { notes } = this.state;
+        const updatedNote = noteData.value.data.onUpdateNote;
+        const index = notes.findIndex(note => note.id === updatedNote.id)
+        const updatedNotes = [
+          ...notes.slice(0, index), //1) The part of the array up until the updated note 
+          updatedNote, //2) The updated note
+          ...notes.slice(index + 1) //3) The part of the array after the updated note
+          // 2nd arg is not required for slice() to get from a certain index to the end
+        ]
+        this.setState({ notes: updatedNotes, note: "", id: "" });
+
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.createNoteListener.unsubscribe(); //Removing the onCreateNote listener, before component is unMounted
+    this.deleteNoteListener.unsubscribe();
+    this.updateNoteListener.unsubscribe();
   }
 
   getNotes = async () => {
@@ -56,11 +85,11 @@ class App extends React.Component {
     } else {
       const input = { note }; // input = { note: note }
       // using async await to receive the response data from the GraphQL mutation
-      await API.graphql(graphqlOperation(createNote, { input })) // var to store the return data from createNode operation is not needed
+      await API.graphql(graphqlOperation(createNote, { input })) // var to store the return data from createNote operation is not needed
       // grapqlOperation( reference to the mutation function, 2nd arg - i/p to the mutation fn(), which should be given in an obj, as the- 
       //- i/p param expected here, as per graphQL is an object )
       // result -> response || all the data is available on the property 'data', in that property, all the note data is available on a -
-      //- property 'createNode', which matches the operation performed here
+      //- property 'createNote', which matches the operation performed here
       // const newNote = result.data.createNote --> this logic has been added in the subscriber
       // const updatedNotes = [newNote, ...notes];
       // this.setState({ notes: updatedNotes, note: "" }); -> updating the notes array is done by the subscriber, and this should be removed
@@ -69,36 +98,32 @@ class App extends React.Component {
     }
   }
 
-  componentWillUnmount() {
-    this.createNoteListener.unsubscribe(); //Removing the onCreateNote listener, before component is unMounted
-  }
-
   handleSetNote = ({ note, id }) => this.setState({ note, id })// id is to find where the note originally was, in the list and add the updated-
   //- note in the same place
 
   handleUpdateNote = async () => {
     const { id, notes, note } = this.state;
     const input = { id, note };
-    const result = await API.graphql(graphqlOperation(updateNote, { input }))
-    const updatedNote = result.data.updateNote;
-    const index = notes.findIndex(note => note.id === updatedNote.id)
-    const updatedNotes = [
-      ...notes.slice(0, index), //1) The part of the array up until the updated note 
-      updatedNote, //2) The updated note
-      ...notes.slice(index + 1) //3) The part of the array after the updated note
-      // 2nd arg is not required for slice() to get from a certain index to the end
-    ]
-    this.setState({ notes: updatedNotes, note: "", id: "" });
+    await API.graphql(graphqlOperation(updateNote, { input }))
+    // const updatedNote = result.data.updateNote;
+    // const index = notes.findIndex(note => note.id === updatedNote.id)
+    // const updatedNotes = [
+    //   ...notes.slice(0, index), //1) The part of the array up until the updated note 
+    //   updatedNote, //2) The updated note
+    //   ...notes.slice(index + 1) //3) The part of the array after the updated note
+    //   // 2nd arg is not required for slice() to get from a certain index to the end
+    // ]
+    // this.setState({ notes: updatedNotes, note: "", id: "" });
   }
 
-  handleDeleteNode = async noteId => {
-    const { notes } = this.state;
+  handleDeleteNote = async noteId => {
+    // const { notes } = this.state;
     const input = { id: noteId };
-    const result = await API.graphql(graphqlOperation(deleteNote, { input }));
+    await API.graphql(graphqlOperation(deleteNote, { input }));
     // deleteNote mutation fn() returns the id of the note/item deleted
-    const deletedNoteId = result.data.deleteNote.id;
-    const updatedNotes = notes.filter(note => note.id !== deletedNoteId);// filter() will filter out the deleted note from notes
-    this.setState({ notes: updatedNotes });
+    // const deletedNoteId = result.data.deleteNote.id;
+    // const updatedNotes = notes.filter(note => note.id !== deletedNoteId);// filter() will filter out the deleted note from notes
+    // this.setState({ notes: updatedNotes });
   }
 
   render() {
@@ -133,8 +158,8 @@ class App extends React.Component {
                 - handleSetNote() is conv as an arrow fn() to prevent it from being called on pageLoad*/}
                 {note.note}
               </li>
-              <button onClick={() => this.handleDeleteNode(note.id)} className="bg-transparent bn f4">
-                {/* this.handleDeleteNode(item.id) is gven as an arrow function, so this won't be called during the page load, since-
+              <button onClick={() => this.handleDeleteNote(note.id)} className="bg-transparent bn f4">
+                {/* this.handleDeleteNote(item.id) is gven as an arrow function, so this won't be called during the page load, since-
                 - arguments are passed onto the function */}
                 <span>&times;</span>
               </button>
