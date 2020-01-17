961238e5c4cac1d03c61ae36c7bc165e6aa9a60d
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-01-17 10:02:36 -0600

    Added a function to delete the notes, using the deleNote mutation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { API, graphqlOperation } from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react'
 
-import { createNote } from './graphql/mutations'
+import { createNote, deleteNote } from './graphql/mutations'
 import { listNotes } from './graphql/queries'
 
 class App extends React.Component {
@@ -24,12 +24,24 @@ class App extends React.Component {
     const input = { note }; // input = { note: note }
     // using async await to receive the response data from the GraphQL mutation
     const result = await API.graphql(graphqlOperation(createNote, { input })) // { input*the property required in the graphql query*: input*var* }
-    // result -> response || all teh data is available on the property 'data', in that property, all the note data is available on a -
+    // grapqlOperation( reference to the mutation function, 2nd arg - i/p to the mutation fn(), which should be given in an obj, as the- 
+    //- i/p param expected here, as per graphQL is an object )
+    // result -> response || all the data is available on the property 'data', in that property, all the note data is available on a -
     //- property 'createNode', which matches teh operation performed here
     const newNote = result.data.createNote
     const updatedNotes = [newNote, ...notes];
     this.setState({ notes: updatedNotes, note: "" });
     // update the notes list along with the latest addition and set the note to empty_string, when calling setState
+  }
+
+  handleDeleteNode = async noteId => {
+    const { notes } = this.state;
+    const input = { id: noteId };
+    const result = await API.graphql(graphqlOperation(deleteNote, { input }));
+    // deleteNote mutation fn() returns the id of the note/item deleted
+    const deletedNoteId = result.data.deleteNote.id;
+    const updatedNotes = notes.filter(note => note.id !== deletedNoteId);// filter() will filter out the deleted note from notes
+    this.setState({ notes: updatedNotes });
   }
 
   render() {
@@ -59,7 +71,9 @@ class App extends React.Component {
               <li className="list pa1 f3">
                 {note.note}
               </li>
-              <button className="bg-transparent bn f4">
+              <button onClick={() => this.handleDeleteNode(note.id)} className="bg-transparent bn f4">
+                {/* this.handleDeleteNode(item.id) is gven as an arrow function, so this won't be called during the page load, since-
+                - arguments are passed onto the funciton */}
                 <span>&times;</span>
               </button>
             </div>
